eaf77f0c753dd7f45c547130e794fa8de793747a
ndurgesh88 <durgeshnidadavolu@gmail.com>
2025-07-04 23:27:51 +0800

    Fix: Approach 2 Covert to Title Case

--- 02-javascript-coding-practice/01-Core JavaScript Mastery/02-strings/solutions/04-convert-to-title-case.js
+++ 02-javascript-coding-practice/01-Core JavaScript Mastery/02-strings/solutions/04-convert-to-title-case.js
@@ -15,3 +15,18 @@ const wordOne = firstLetter + words[0].slice(1);
 const wordTwo = secondLetter + words[1].slice(1);
 const finalStrOutput = wordOne + ' ' + wordTwo;
 console.log(finalStrOutput);
+
+/*
+ Approach 2 
+ Split → Capitalize first letter + rest → Join
+ */
+
+function firstletterCapitalizedStr(str) {
+  const words = str
+    .split(' ')
+    .map((item) => item.charAt(0).toUpperCase() + item.slice(1));
+  const resultOfTitleCase = words.join(' ');
+  console.log(resultOfTitleCase);
+}
+
+firstletterCapitalizedStr('testing hello');
