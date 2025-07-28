90966295afe05fd116917856387f78715032eb33
ndurgesh88 <durgeshnidadavolu@gmail.com>
2025-07-28 23:34:10 +0800

    Solution fix: 11-replace-word-in-string

---
+++ 02-javascript-coding-practice/01-Core JavaScript Mastery/02-strings/solutions/11-replace-word-in-string.js
@@ -0,0 +1,5 @@
+const strName = 'Hello Hello javascript world';
+const replaceStrWord = strName.replace(/Hello/g, 'Hey');
+console.log(replaceStrWord);
+
+//The g flag represents the global search, meaning it will search the entire string for all instances of the word and replace them.
