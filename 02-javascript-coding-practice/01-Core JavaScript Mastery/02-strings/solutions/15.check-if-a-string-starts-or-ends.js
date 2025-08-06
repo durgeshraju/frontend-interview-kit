cb92663555ed267b7f5b24905d4b6c84c013fbc8
ndurgesh88 <durgeshnidadavolu@gmail.com>
2025-08-06 17:01:15 +0800

    check if a string starts or ends with a specific substring

---
+++ 02-javascript-coding-practice/01-Core JavaScript Mastery/02-strings/solutions/15.check-if-a-string-starts-or-ends.js
@@ -0,0 +1,4 @@
+// How do you check if a string starts or ends with a specific substring?
+const str = 'Javascript';
+console.log('str is starts:', typeof str.startsWith('Java')); // true
+console.log('str is ends:', str.endsWith('script')); // true
