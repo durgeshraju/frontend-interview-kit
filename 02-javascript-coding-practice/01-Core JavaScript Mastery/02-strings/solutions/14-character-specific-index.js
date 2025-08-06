3b026cc21dfb718c711beacba7b82006679c4d93
ndurgesh88 <durgeshnidadavolu@gmail.com>
2025-08-06 16:44:29 +0800

    get the character at a specific index

---
+++ 02-javascript-coding-practice/01-Core JavaScript Mastery/02-strings/solutions/14-character-specific-index.js
@@ -0,0 +1,12 @@
+// How do you get the character at a specific index?
+
+const strCharacterIndex = 'Javascript';
+console.log('strCharacterIndex is:', strCharacterIndex.charAt(0));
+
+// Using charAt() based on zero 0 1, 2, 3 you can get specific charcter
+
+// Here is another example.
+
+const strIndex = 'Hello Javascript';
+const splitStr = strIndex.split('');
+console.log('splitStr is:', splitStr[4]);
